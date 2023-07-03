1021f9649ee7cf9c43808d028c34ba22de168a00
tuxymag <138219744+tuxymag@users.noreply.github.com>
2023-07-03 14:10:17 +0530

    Update index.js - Change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Magin George - Tools -- Rolling" }');
+    res.send('{ "response": "Hello From Magin George - Tools -- Rolling Change" }');
 });
 
 app.get('/will', function (req, res) {

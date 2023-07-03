c7dfbcc1a9b7374388d670c850eab4ee66023eb1
tuxymag <138219744+tuxymag@users.noreply.github.com>
2023-07-03 13:55:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Magin George - Tools" }');
+    res.send('{ "response": "Hello From Magin George - Tools -- Rolling" }');
 });
 
 app.get('/will', function (req, res) {
